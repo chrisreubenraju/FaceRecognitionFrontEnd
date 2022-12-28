18e9e8784fe261090b03761eb7a6f0844e18fbc3
chrisreubenraju <chris.reuben04@gmail.com>
2022-12-28 20:45:29 +0530

    changes to deploy

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ class App extends Component {
       .then(response => {
         console.log('hi', response)
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://facerecognitionbackend.onrender.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
